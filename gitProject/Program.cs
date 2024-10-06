64dc8d1d4b45188ac3468e0c21af307f7cfb028d
shubhamb19 <shubhambhuse19@gmail.com>
2024-10-06 13:48:12 +0530

    fkt program cs file pushed

---
+++ gitProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gitProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
